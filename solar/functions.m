de641f4e0a07f16d7308743a4712f06427501edb
dmvoit <dmvoit.dev@gmail.com>
2023-06-19 00:44:05 +0200

    CameraMatrix changes

--- solar/functions.m
+++ solar/functions.m
@@ -11,13 +11,23 @@ reverseYAxis[ySize_][xy_] := TransformationFunction[{
   {1, 0, 0}, {0, -1, ySize}, {0, 0, 1}}][xy]
 
 PackageExport["CameraMatrix"]
-CameraMatrix[{width_, height_}] := {
-  {height, 0, height / 2},
-  {0, height, width / 2},
+CameraMatrix::usage = "CameraMatrix approximation"
+CameraMatrix[{width_, height_}, scale : 1] := {
+  {scale * height, 0, width / 2},
+  {0, scale * height, height / 2},
   {0, 0, 1}
 }
 
-CameraMatrix[img_Image] := CameraMatrix[ImageDimensions@img]
+CameraMatrix[img_Image, scale : 1] := CameraMatrix[ImageDimensions@img, scale]
+
+CameraMatrixOrientation[K_] := If[
+  K[[1, 3]]/K[[2, 3]] > 1,
+  "Landscape", "Portrait"]
+
+CameraMatrixFlip[{{fx_, s_, cx_}, {_, fy_, cy_}, {__}}] := {
+  {fy, s, cy},
+  {0, fx, cx},
+  {0, 0, 1}}
 
 PackageExport["pointsCompare"]
 pointsCompare[img_, data_ : {{ls_List -> name_String}..}] :=
